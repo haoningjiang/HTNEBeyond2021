c127296c5876057c75e6b5d95d12706ec9fa4703
haoningjiang <75537462+haoningjiang@users.noreply.github.com>
2021-01-17 01:53:02 -0800

    Update splitData.m

--- prepare-data/splitData.m
+++ prepare-data/splitData.m
@@ -1,13 +1,16 @@
 function [ytr, ycv, yts, Xa, Xb, Xc, Xd, Xe, Xatr, Xacv, Xats, Xbtr, Xbcv, Xbts, ...
     Xctr, Xccv, Xcts, Xdtr, Xdcv, Xdts, Xetr, Xecv, Xets] ...
     = splitData(X, y)
+%split data into training, cross validation, test set
+%also create different X's with different features (ie. polynomial ones) to test multiple at once
+
 
 ytr = y([1:900], :);
 ycv = y([901:1186], :);
 yts = y([1187:end], :);
 
 
-
+%ended up going with this one
 Xa = [X(:, [1, 2, 3, 4]) ((X(:, 1) .^2)./100) ((X(:, 2) .^2)./10) ...
     ((X(:, 2) .^3) / 10) (X(:, 3) .^ 3) ((X(:, 5) .^ 3) ./ 100)];
 
