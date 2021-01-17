57cb9caf94688fabb15d14703134b3f3626566ec
haoningjiang <75537462+haoningjiang@users.noreply.github.com>
2021-01-17 01:46:35 -0800

    Update getTheta.m

--- neural-network/getTheta.m
+++ neural-network/getTheta.m
@@ -5,7 +5,7 @@ function [Theta1 Theta2] = getTheta(X, y, lambda, layer_size)
 %parameters obtained by training the neural network using the training
 %data. 
 
-%shorthand for neuralcostfunc, that only takes in the nn param as input
+%shorthand for neuralcostfunc, that only takes in theta as input
 costFunction = @(p) neuralCostFunc(p, X, y, lambda, layer_size);
 
 %random initial thetas to to avoid symmetry issue
@@ -13,12 +13,6 @@ init_Theta1 = rand(layer_size, layer_size + 1) * 2 * 0.12 - 0.12;
 init_Theta2 = rand(1, layer_size + 1) * 2 * 0.12 - 0.12;
 
 init_theta = [init_Theta1(:) ; init_Theta2(:)];
-
-%find theta by minimizing cost function
-%options = optimset('MaxIter', 50);
-%[theta, ~] = fmincg(costFunction, init_theta, options);
-
-%options = optimoptions(@fminunc,'Algorithm','trust-region','SpecifyObjectiveGradient',true);
 
 options = optimoptions(@fminunc, 'Display', 'iter', 'Algorithm', 'trust-region', ...
     'SpecifyObjectiveGradient', true, 'StepTolerance', 1e-8, 'CheckGradients', true);
