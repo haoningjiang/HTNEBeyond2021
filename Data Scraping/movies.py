61e7ba6eeb4bc13e1bd8215b0343c4c027f033c1
lindaf3 <57429580+lindaf3@users.noreply.github.com>
2021-01-16 22:41:35 -0800

    Add Data Scraping

---
+++ Data Scraping/movies.py
@@ -0,0 +1,24 @@
+#Created by Linda Fan and Haoning Jiang
+def import_all_movies():
+    '''Returns the Bechdel Tests' API's Movies' data'''
+    api_url = "http://bechdeltest.com/api/v1/getAllMovies?"
+    url_feedback = None
+    try:
+        url_feedback = urllib.request.urlopen(api_url)
+        json_file = url_feedback.read().decode(encoding = 'utf-8')
+        return json.loads(json_file)
+    finally:
+        if url_feedback != None:
+            url_feedback.close()
+
+def write_all_movies():
+    '''Writes the movies's imbid and Bechdel Rating into a csv_file'''
+    with open('all_movies.csv', mode = 'w') as csv_file:
+        fieldnames = ['imdb_id', 'rating']
+        writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
+        writer.writeheader()
+        all_movies = import_all_movies()
+        for movie in all_movies:
+            imdb_id = movie["imdbid"] 
+            rating = movie["rating"]
+            writer.writerow({'imdb_id': imdb_id, 'rating': rating})
