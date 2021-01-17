8f6bfac00a9575a402852db3f3514259756f50d7
haoningjiang <75537462+haoningjiang@users.noreply.github.com>
2021-01-17 01:50:24 -0800

    Update lrPredict.m

--- logistic-reg/lrPredict.m
+++ logistic-reg/lrPredict.m
@@ -3,17 +3,6 @@ function p = lrPredict(theta, X, threshold)
 m = size(X, 1); % Number of training examples
 X = [ones(m, 1) X];
 
-%p = zeros(m, 1);
-
-%h = sigmoid(X * theta);
-
-%for i = 1:m
-    %if h(m) >= 0.1
-    %    p(m) = 1;
-    %end
-    %p(m) = h(m);
-%end
-
 p = sigmoid(X * theta);
 p(p >= threshold) = 1;
 p(p < threshold) = 0;
