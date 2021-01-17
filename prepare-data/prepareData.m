6386a895159024b894552712995a6a7b9fa2d7e8
haoningjiang <75537462+haoningjiang@users.noreply.github.com>
2021-01-17 01:51:30 -0800

    Update prepareData.m

--- prepare-data/prepareData.m
+++ prepare-data/prepareData.m
@@ -1,10 +1,12 @@
 function [X, y] = prepareData(file)
+%call with movie_data.txt 
 
 moviedata = dlmread(file);
 y = moviedata(:, end);
 x = moviedata(:, [1:4]);
 m = length(y);
 
+%add column for ratio
 X = [x (x(:, 2) ./ (x(:, 1) + x(:, 2)))];
 X(:, 3) = X(:, 3) / 1000;
 X(:, 4) = X(:, 4) / 10;
